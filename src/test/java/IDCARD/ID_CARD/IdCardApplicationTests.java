bfbee04c44343d97be0ccc6531a5c8ef903c2a8b
dhiastiii <dhiastiputri.jayanti@gmail.com>
2022-11-15 16:21:02 +0700

    FIRST COMMIT

---
+++ src/test/java/IDCARD/ID_CARD/IdCardApplicationTests.java
@@ -0,0 +1,13 @@
+package IDCARD.ID_CARD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
